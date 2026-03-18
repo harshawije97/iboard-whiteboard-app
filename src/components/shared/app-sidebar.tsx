61a8dcbf315a118cb55e452ffd262340657585e2
harshawije97 <harshawije99@gmail.com>
2026-03-19 00:54:07 +0530

    organizations completed

--- src/components/shared/app-sidebar.tsx
+++ src/components/shared/app-sidebar.tsx
@@ -3,6 +3,12 @@
 import React from 'react'
 import NewActionButton from '../organization/new-button';
 import OrganizationList from '../organization/org-list';
+import Link from 'next/link';
+import Image from 'next/image';
+import { OrganizationSwitcher } from '@clerk/nextjs';
+import { Button } from '../ui/button';
+import { LayoutDashboard, Star } from 'lucide-react';
+import { usePathname, useSearchParams } from 'next/navigation';
 
 function AppSidebar() {
     return (
@@ -14,8 +20,64 @@ function AppSidebar() {
 }
 
 export function OrganizationSidebar() {
+    const pathName = usePathname();
+    const searchParams = useSearchParams();
+
+    const path = pathName.split("/")[1];
+    const favorites = searchParams.get("favorite");
+
+
     return (
-        <aside className='hidden lg:flex flex-col space-y-6 w-51.5 pl-5 pt-5 bg-accent-foreground/5'>OrganizationSidebar</aside>
+        <aside className='hidden lg:flex flex-col space-y-6 w-51.5 pl-5 pt-5'>
+            <Link href={`/${path}`}>
+                <div className='flex items-center gap-x-2'>
+                    <Image src="/logo.svg" alt="logo" width={25} height={25} />
+                    <span className="font-semibold text-2xl text-slate-700">
+                        iBoard
+                    </span>
+                </div>
+            </Link>
+            <OrganizationSwitcher hidePersonal
+                appearance={{
+                    elements: {
+                        rootBox: {
+                            display: "flex",
+                            justifyContent: "center",
+                            alignItems: "center",
+                            width: "100%",
+                        },
+                        organizationSwitcherTrigger: {
+                            padding: "6.5px",
+                            width: "100%",
+                            borderRadius: "4px",
+                            backgroundColor: "hsl(var(--primary))",
+                            color: "hsl(var(--primary-foreground))",
+                            border: "1px solid #e5e7eb",
+                            justifyContent: "center",
+                        }
+                    }
+                }} />
+            <div className='space-y-1 w-full'>
+                <Button variant={favorites ? "ghost" : "secondary"} asChild className='font-normal justify-start px-2 w-full'>
+                    <Link href={{
+                        pathname: `/${path}`,
+                        query: { teamBoards: "true" }
+                    }}>
+                        <LayoutDashboard className='size-4 mr-2' />
+                        Team Boards
+                    </Link>
+                </Button>
+                <Button variant={favorites ? "secondary" : "ghost"} asChild className='font-normal justify-start px-2 w-full'>
+                    <Link href={{
+                        pathname: `/${path}`,
+                        query: { favorite: "true" }
+                    }}>
+                        <Star className='size-4 mr-2' />
+                        Favorites
+                    </Link>
+                </Button>
+            </div>
+        </aside>
     )
 }
 
